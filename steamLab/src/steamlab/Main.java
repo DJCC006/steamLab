f39940e64460cf9fba27df701ab23faf2822f0aa
ljmc <ljmc2304@gmail.com>
2025-11-28 10:21:38 -0600

    desarrollo de frames

--- steamLab/src/steamlab/Main.java
+++ steamLab/src/steamlab/Main.java
@@ -4,6 +4,7 @@
  */
 package steamlab;
 
+import java.io.IOException;
 import javax.swing.SwingUtilities;
 
 /**
@@ -14,36 +15,20 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    /*
-    public boolean playerExists(String username) throws IOException {
-    player.seek(0);
-
-    while (player.getFilePointer() < player.length()) {
-        int code = player.readInt();
-        String user = player.readUTF();
-        String pass = player.readUTF();
-        String name = player.readUTF();
-        long nac = player.readLong();
-        int score = player.readInt();
-
-        // Leer imagen (debes conocer el tamaño fijo, si no lo usas, ignóralo)
-        byte[] img = new byte["];
-        player.read(img);
-
-        String tipo = player.readUTF();
-
-        if (user.equals(username)) {
-            return true;
-        }
-    }
-    */
     public static void main(String[] args) {
         
         steam st=new steam();
-        byte[] img = .readAllBytes(
-            Paths.get("C:\\Users\\ljmc2\\Documents\\steamLab\\steamLab\\src\\res\\defaultImg.jpg")
-        );
-        st.addPlayer("admin", "password", "Administrador Default", 1,img, "admin");
+        try{//administrador default
+            st.addPlayer("admin", 
+                    "password", 
+                    "Administrador Default", 
+                    1,
+                    "src\\res\\defaultImg.jpg", 
+                    "admin");
+        
+        }catch(IOException e){
+            e.getMessage();
+        }
         SwingUtilities.invokeLater(() -> new loginFrame().setVisible(true));
     }
     
